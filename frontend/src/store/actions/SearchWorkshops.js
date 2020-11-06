d6bb7c5ad99fd72a1a8c342e26a00e72e9291275
Matias <mlavin7@gmail.com>
2020-11-06 17:07:58 +0100

    updating HOC from development

--- frontend/src/store/actions/SearchWorkshops.js
+++ frontend/src/store/actions/SearchWorkshops.js
@@ -1,58 +1,20 @@
 import React, { useState, useEffect} from "react"; 
 
-
-export const WorkshopSearch = () => ({
-  type: 'WORKSHOPS_FILTER',
-  payload: workshop
+export const actionName = data => ({
+  type: "WORKSHOPS_FILTER", 
+  payload: data
 })
+// ^= (up)rx action , connect with file  actionTypes  "WORKSHOPS_FILTER"
+// now lets define the action =v (down)
 
-const initialState = {
 
+
+
+export const SearchWorkshopAction = workshopData => async (dispatch, getState) => {
+  
+
+  const [state, setState] = useState(initialState);
+
+  return <Fragment>content</Fragment>;
 }
-
-import { useReducer } from "react";
-
-/*
-* => Lazy initialization
-* => You can also create an initial state lazily. To do this,
-* => you can pass the init function as the third argument.
-* => The initial state will be set equal to the result of calling init (initialArg).
-* => This allows you to extract the logic for calculating the initial state outside the reducer. 
-* => It is also convenient for resetting later in response to an action.
-* => source: https://en.reactjs.org/docs/hooks-reference.html#useReducer
-*/
-
-function init(initialCount) {
-  return {count: initialCount};
-}
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'increment':
-      return {count: state.count + 1};
-    case 'decrement':
-      return {count: state.count - 1};
-    case 'reset':
-      return init(action.payload);
-    default:
-      throw new Error();
-  }
-}
-
-function Counter({initialCount}) {
-  const [state, dispatch] = useReducer(reducer, initialCount, init);
-  return (
-    <>
-      Count: {state.count}
-      <button
-        onClick={() => dispatch({type: 'reset', payload: initialCount})}>
-
-        Reset
-      </button>
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-      <button onClick={() => dispatch({type: 'increment'})}>+</button>
-    </>
-  );
-}
-
 
