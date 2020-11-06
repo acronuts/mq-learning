14629452bf71a9215e723dd02fed7086c543e3a6
Matias <mlavin7@gmail.com>
2020-11-06 14:29:05 +0100

    updating barnch

--- frontend/src/store/actions/SearchWorkshops.js
+++ frontend/src/store/actions/SearchWorkshops.js
@@ -1,41 +1,58 @@
 import React, { useState, useEffect} from "react"; 
 
 
+export const WorkshopSearch = () => ({
+  type: 'WORKSHOPS_FILTER',
+  payload: workshop
+})
+
+const initialState = {
+
+}
 
 import { useReducer } from "react";
 
 /*
-* => The useReducer hook is usually preferable to useState when you have complex state logic
-* => that includes multiple values, or when the next state depends on the previous one.
-* => useReducer also allows you to optimize the performance of components that trigger deep updates, 
-* => since you can pass dispatch instead of callbacks.
-* => React ensures that the identity of the dispatch function is stable and does not change with repeated renders.
-* => Therefore, it can be safely omitted from the useEffect and useCallback hook dependency lists.
-* => If you return the same value from the hook reducer as the current state,
-* => React will exit without redrawing the children or triggering effects. (React uses the Object.is comparison algorithm.)
-* => If you do expensive calculations during rendering, you can optimize them with useMemo
-* => source: https://en.reactjs.org/docs/hooks-reference.html#usereducer
-
+* => Lazy initialization
+* => You can also create an initial state lazily. To do this,
+* => you can pass the init function as the third argument.
+* => The initial state will be set equal to the result of calling init (initialArg).
+* => This allows you to extract the logic for calculating the initial state outside the reducer. 
+* => It is also convenient for resetting later in response to an action.
+* => source: https://en.reactjs.org/docs/hooks-reference.html#useReducer
 */
 
-const initialState = {count: 0};
+function init(initialCount) {
+  return {count: initialCount};
+}
+
 function reducer(state, action) {
   switch (action.type) {
     case 'increment':
       return {count: state.count + 1};
     case 'decrement':
       return {count: state.count - 1};
+    case 'reset':
+      return init(action.payload);
     default:
       throw new Error();
   }
 }
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+
+function Counter({initialCount}) {
+  const [state, dispatch] = useReducer(reducer, initialCount, init);
   return (
     <>
       Count: {state.count}
+      <button
+        onClick={() => dispatch({type: 'reset', payload: initialCount})}>
+
+        Reset
+      </button>
       <button onClick={() => dispatch({type: 'decrement'})}>-</button>
       <button onClick={() => dispatch({type: 'increment'})}>+</button>
     </>
   );
 }
+
+
